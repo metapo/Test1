bcaf579094441380315ab7fa37f8adb907433979
mehtavpou <mehtavpou@gmail.com>
2023-05-29 00:41:17 +0330

    added some env key, updated documetation

--- config/device-scanner.php
+++ config/device-scanner.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'driver' => env('DEVICE_SCANNER', 'nmap'),
-    'ip_range' => '192.168.1.1',
-    'timeout' => 120
+    'driver' => env('DEVICE_SCANNER_DRIVER', 'nmap'),
+    'ip_range' => env('NMAP_IP_RANGE', '192.168.1.1'),
+    'timeout' => env('NMAP_TIMEOUT', 120)
 ];
